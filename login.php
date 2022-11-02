ba0389fe736b8fd467c5d1e2decc3d053416b40e
nicolinesh <112643950+nicolinesh@users.noreply.github.com>
2022-11-02 11:32:54 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,40 +6,39 @@ session_start();
 	include("functions.php");
 
 
-	if($_SERVER['REQUEST_METHOD'] == "POST")
+	if($_SERVER['REQUEST_METHOD'] == "POST") // Her tjekkes, om brugeren har trykket på "login"-knappen
 	{
 		
 		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name)) // Vi tjekker, om user_name og password er udfyldt, og at brugernavnet ikke kun består af tal
 		{
 
-			
-			$query = "select * from users where user_name = '$user_name' limit 1";
-			$result = mysqli_query($con, $query);
+			// Hvis ovenstående er udfyldt læses informationerne fra databasen
+			$query = "select * from users where user_name = '$user_name' limit 1"; // Vi sammenligner, om det brugernavn der er indtastet af brugeren, findes i databasen. "Limit 1" sørger for, at vi kun får 1 resultat
+			$result = mysqli_query($con, $query); // Skrives for at få resultatet  
 
-			if($result)
+			if($result) // Vi tjekker, om resultatet er rigtigt
 			{
-				if($result && mysqli_num_rows($result) > 0)
+				if($result && mysqli_num_rows($result) > 0) // Hvis resultatet er positivt, og antallet af rækker er større end 0, hentes data ved nedenstående kode
 				{
 
 					$user_data = mysqli_fetch_assoc($result);
 					
-					if($user_data['password'] === $password)
+					if($user_data['password'] === $password) // Hvis passwordet i databasen er det samme, som det password brugeren har givet.. se næste kommentar
 					{
 
 						$_SESSION['user_id'] = $user_data['user_id'];
-						header("Location: index.php");
+						header("Location: index.php"); // Hvis alt virker, omdirigeres brugeren til index-siden
 						die;
 					}
 				}
 			}
 			
-			echo "Forkert brugernavn eller kodeord!";
 		}else
 		{
-			echo "Forkert brugernavn eller kodeord!";
+			echo "Forkert brugernavn eller kodeord!"; // Udskriver denne sætning, hvis ovenstående ikke er opfyldt. 
 		}
 	}
 
@@ -85,14 +84,14 @@ session_start();
 
 	<div id="box">
 		
-		<form method="post">
+		<form method="post"> <!-- De felter der bliver tjekket i ovenstående kode -->
 			<div style="font-size: 20px;margin: 10px;color: white;">Log ind</div>
 
 			<input id="text" type="text" name="user_name"><br><br>
 			<input id="text" type="password" name="password"><br><br>
-			<input id="button" type="submit" value="Log ind"><br><br>
+			<input id="button" type="submit" value="Log ind"><br><br> <!-- Login-knap -->
 
-			<a href="signup.php">Tilmeld dig</a><br><br>
+			<a href="signup.php">Tilmeld dig</a><br><br> <!-- Link til signup-siden -->
 		</form>
 	</div>
 </body>
