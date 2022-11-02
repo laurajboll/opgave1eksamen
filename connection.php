ef0d6f4db76d789ee7f3582445f77a695bd8b190
laurajboll <112655987+laurajboll@users.noreply.github.com>
2022-11-02 09:37:25 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,14 @@
 <?php
-
+// På de næste 4 linjer defineres variabler til host, username, password og database navnet for at oprette forbindelse.
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "root";
 $dbname = "login_sample_db";
 
+// Hvis der ikke oprettes forbindelse bliver udskrevet 'Kunne ikke oprette forbindelse'. 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname))
 
+// Die er alias for exit() function. Denne printer en besked og afslutter scriptet.
 {
-    die("failed to connect!");
+    die("Kunne ikke oprette forbindelse!");
 }
