f89b4253b62c12cf5364f854206c35d7b19069db
nicolinesh <112643950+nicolinesh@users.noreply.github.com>
2022-11-02 11:36:35 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,11 +2,11 @@
 
 session_start();
 
-if(isset($_SESSION['user_id']))
+if(isset($_SESSION['user_id'])) // Tjekker om værdien 'user_id' er aktiviveret
 {
-	unset($_SESSION['user_id']);
+	unset($_SESSION['user_id']); // Hvis ovenstående er gældende, deaktiveres id'et
 
 }
 
-header("Location: login.php");
+header("Location: login.php"); // Brugeren omdirigeres til login-siden
 die;
